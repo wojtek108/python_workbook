6470c0769b38e570f61d2f5871a5525f64dc742a
wojtek108 <44767709+wojtek108@users.noreply.github.com>
2022-07-14 16:45:31 +0200

    Add files via upload

--- wordListAverage.py
+++ wordListAverage.py
@@ -13,3 +13,14 @@ def wordList(lista):
 
 print(wordList(['tomek', 'michal', 'wg', 'michalski']))
 
+
+# I didn´t know the min() and max() methods which make for a more pythonic solution as demonstrated by Reuven
+
+def summarize(words):
+    word_lengths = [len(one_word)
+                    for one_word in words]
+
+    return min(word_lengths), max(word_lengths), sum(word_lengths)/len(word_lengths)
+
+
+print(summarize(['tomek', 'michal', 'wg', 'michalski']))
